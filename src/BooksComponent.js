616dd2401863c89eb0b55f45ae12274b1bfbc175
Guillaume Percepied <gcconantc@gmail.com>
2020-02-04 14:22:32 +0100

    Update BooksComponent and add api token

--- src/BooksComponent.js
+++ src/BooksComponent.js
@@ -3,6 +3,9 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import CardDeck from "react-bootstrap/CardDeck";
 import { withCookies } from 'react-cookie';
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Container from "react-bootstrap/Container";
 
 class BooksComponent extends React.Component {
     constructor(props) {
@@ -15,10 +18,9 @@ class BooksComponent extends React.Component {
     }
 
     componentDidMount() {
-        //let authToken = Cookies.get("authToken");
-        let headerObj = {'x-auth-token': "authToken"};
-
-        fetch("http://sf5.api.local.wip:8000/books")
+        fetch("http://sf5.api.local.wip:8000/api/books", {
+            headers: { 'X-AUTH-TOKEN': "_apiTokenXYZ_@321" }
+        })
             .then(res => res.json())
             .then(
                 (result) => {
@@ -44,22 +46,32 @@ class BooksComponent extends React.Component {
             return <div>Chargement…</div>;
         } else {
             return (
+                <Container>
+
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+
                     <CardDeck>
-                { items.map((item) => (
-                    <Card style={{ width: '18rem' }} key={item.isbn}>
-                        <Card.Body>
-                            <Card.Title>{item.author}</Card.Title>
-                            <Card.Text>
-                                {item.title}
-                            </Card.Text>
-                            <Button variant="primary">See details</Button>
-                        </Card.Body>
-                        <Card.Footer>
-                            <small className="text-muted">New !</small>
-                        </Card.Footer>
-                    </Card>
-                ))}
+
+                        { items.map((item) => (
+                            <Card style={{ width: '18rem' }} key={item.isbn}>
+                                <Card.Body>
+                                    <Card.Title>{item.author}</Card.Title>
+                                    <Card.Text>
+                                        {item.title}
+                                    </Card.Text>
+                                    <Button variant="primary">See details</Button>
+                                </Card.Body>
+                                <Card.Footer>
+                                    <small className="text-muted">New !</small>
+                                </Card.Footer>
+                            </Card>
+                        ))}
                     </CardDeck>
+
+                </Container>
             );
         }
     }
